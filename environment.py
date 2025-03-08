a54248ca41dbc0900d3f172ca4824fc0ebe758ed
betato <battlestargatticus@gmail.com>
2025-03-08 18:04:12 -0500

    correrct eccentricity

--- environment.py
+++ environment.py
@@ -10,7 +10,8 @@ G_E = 9.7803253359*ur.meter*ur.second**-2					# gravity at equator		[m s^-2]
 G_P = 9.8321849378*ur.meter*ur.second**-2 					# gravity at poles			[m s^-2]
 A = 6378137*ur.meter										# earth semi-major axis		[m]
 B = 6356752*ur.meter										# earth semi-minor axis		[m]
-E = (A**2-B**2)/(A**2)										# earth eccentricity
+F = 1 - B/A													# earth flattening
+E = (1 - B**2/A**2)**0.5									# earth eccentricity
 GM = G*M													# gravity formula constant	[N m^2 kg^-1]
 K = (B*G_P - A*G_E)/(A*G_E)									# gravity formula constant	[-]
 
@@ -21,23 +22,25 @@ class Environment:
 		pass
 
 	def geodetic_to_ecef(self, lat:float, long:float, alt:float) -> tuple[float, float, float]:
-		print(lat, long)
+		print('lat, long:',lat, long)
 		# convert west (negative) longditude to east
 		if long < 0:
-			long = 360+long
-		print(lat, long)
+			long = 360*ur.degree + long
+		print('lat, long:',lat, long)
 		# convert from degrees to radians
 		phi = np.deg2rad(lat)
 		lam = np.deg2rad(long)
-		print(phi, lam)
+		print('phi, lam:',phi, lam)
 		# prime vertical radius of curvature
-		n = A**2/(A**2*math.cos(phi)**2 + B**2*math.sin(phi))**0.5
-		print(n)
+		n = A**2/(A**2*np.cos(phi)**2 + B**2*np.sin(phi))**0.5
+		print('n',n)
+		n = A/(1-E**2*np.sin(phi)**2)
+		print('n',n)
 		# earth centered earth fixed cooordinates (origin at center of earth)
 		# z through ellipsoid north pole, x through intersection of equator and prime meridian
-		x = (n+alt)*math.cos(phi)*math.cos(lam)
-		y = (n+alt)*math.cos(phi)*math.sin(lam)
-		z = ((1-E**2)*n+alt)*math.sin(phi)
+		x = (n+alt)*np.cos(phi)*np.cos(lam)
+		y = (n+alt)*np.cos(phi)*np.sin(lam)
+		z = ((1-E**2)*n+alt)*np.sin(phi)
 		return (x, y, z)
 
 	def g(self, lat:float, long:float, alt:float) -> float:
@@ -47,7 +50,7 @@ class Environment:
 		# https://mwrona.com/posts/gravity-models/
 		# https://en.wikipedia.org/wiki/Theoretical_gravity#Somigliana_equation
 		# Somigliana equation with WGS84 parameters 
-		g = G_E * ((1+K*math.sin(math.radians(lat))**2)/math.sqrt(1-E**2))
+		g = G_E * ((1+K*np.sin(math.radians(lat))**2)/math.sqrt(1-E**2))
 		# free-air gravity correction for altitude 
 		g_loss = GM/(A+alt)**2 - GM/A**2
 		return -(g - g_loss)
@@ -85,14 +88,16 @@ class Environment:
 if __name__ == "__main__":
 	env = Environment()
 
-	# Test gravity
 	lat = 47.986943*ur.degree		# [deg]
 	long = -81.848339*ur.degree		# [deg]
 	alt = 200*ur.meter				# [m ASL]
 
 	ecef = env.geodetic_to_ecef(lat, long, alt)
-	print('x:{}, y:{}, z:{}'.format(round(ecef[0]/1000), round(ecef[1]/1000), round(ecef[2]/1000)))
+	print('x:{}, y:{}, z:{}'.format(round(ecef[0]), round(ecef[1]), round(ecef[2])))
 
+	print('eccentricity:', E)
+
+	# Test gravity
 	print(env.g(lat, long, alt))
 
 	# Test temperature
