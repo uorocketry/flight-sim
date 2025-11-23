a63872491eea12f2f597c64a5f7bb40d2bdec794
betato <battlestargatticus@gmail.com>
2025-11-22 22:11:24 -0500

    Add motor files

--- solid_motor.py
+++ solid_motor.py
@@ -1,4 +1,19 @@
+import xml.etree.ElementTree as ET
+from rigid_body import RigidBody
 
 class SolidMotor:
-	def __init__(self):
+	def __init__(self, file):
+		tree = ET.parse(file)
+		for elem in tree.iter():
+			print(elem.tag, elem.attrib)
+
+	def get_thrust(self, t) -> float:
+		"""
+		Motor thrust in N vs time since ignition
+		"""
 		pass
+
+if __name__ == '__main__':
+	print('------ testing solid_motor.py ------')
+
+	SolidMotor('motor_files/Cesaroni_9977M2245-P.rse')
