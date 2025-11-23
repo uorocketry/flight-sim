3742c8bab9268e5cf64b6d903ffe014d95025509
betato <battlestargatticus@gmail.com>
2025-11-22 23:40:14 -0500

    Test rk4 solver

--- flight_sim.py
+++ flight_sim.py
@@ -3,32 +3,40 @@ from scipy import integrate
 import math
 import matplotlib.pyplot as plt
 
-from rigid_body import RigidBody
+from rocket import Rocket
 from environment import Environment
 
+# simulation
+end_time = 10			# [s]
 
 # launch site
-lat = 47.986943			# [deg]
-long = -81.848339		# [deg]
-alt = 200				# [m]
+lat0 = 47.986943			# [deg]
+lon0 = -81.848339		# [deg]
+alt0 = 200				# [m]
+v0 = 60					# [m/s]
 
-env = Environment(lat, long, alt)
-rocket = RigidBody()
-
-print(env.gravity(lat, long, alt))
+# set up initial rocket conditions
+env = Environment(lat0, lon0, alt0)
+rocket = Rocket()
+rocket.x[1] = alt0
+rocket.p[1] = v0*rocket.m
 
 def step(t, state) -> np.array:
-	g = [0, env.gravity(lat, long, alt), 0]
-	rocket.x[1]
+	g = [0, env.gravity(lat0, lon0, rocket.x[1]), 0]
 	rocket.apply_force(g)
 	return rocket.step(t, state)
 
-solver = integrate.RK45(step, 0, rocket.get_state(), 10, 0.01, )
+# run simulation
+solver = integrate.RK45(step, 0, rocket.get_state(), end_time, 0.01, )
 
-i = 0
+plt_t = []
+plt_y = []
 while solver.t < solver.t_bound:
+	plt_t.append(solver.t)
+	plt_y.append(rocket.x[1])
 	solver.step()
-	i += 1
 
-print(i)
-print(rocket.x)
+plt.plot(plt_t, plt_y)
+plt.xlabel('time (s)')
+plt.ylabel('altitude (m)')
+plt.show()
--- rocket.py
+++ rocket.py
@@ -1,16 +1,17 @@
 import numpy as np
 from solid_motor import SolidMotor
 from rigid_body import RigidBody
+from parachute import Parachute
 import aerodynamics
 
 class Rocket(RigidBody):
 	def __init__(self) -> None:
-		self.body = RigidBody()
+		self.parachutes = []
+		super().__init__()
 
 	def step(self, t, state) -> np.array:
-		# TODO: override rigidbody step
-		# also apply aero loads, thrust, parachute loads, etc.
-		pass
+		# TODO: apply aero loads, thrust, parachute loads
+		return super().step(t, state)
 
 if __name__ == '__main__':
 	print('------ testing rocket.py ------')
